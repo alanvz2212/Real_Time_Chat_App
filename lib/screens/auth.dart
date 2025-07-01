0b797cdf447ed71c6eb6c6bf03e95b95e1cce836
Alan Varghese <alanvz2212@example.com>
2025-07-01 22:58:09 +0530

    Updated

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,6 +8,23 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_emailController);
+      print(_passwordController);
+      // Handle login or sign up logic here
+      print('Form submitted successfully');
+    } else {
+      print('Form validation failed');
+    }
+  }
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +41,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   bottom: 20,
                 ),
                 width: 100,
-                // child: Image.asset('assets/images/whatsapp-7466235.svg'),
+                child: Image.asset('assets/images/whatsapp-873316_1280.png'),
               ),
               Card(
                 margin: EdgeInsets.all(20),
@@ -32,6 +49,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -40,10 +58,54 @@ class _AuthScreenState extends State<AuthScreen> {
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value!.isEmpty ||
+                                  !value.contains('@') ||
+                                  !value.contains('.')) {
+                                return 'Please enter a valid email';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _emailController.text = value!;
+                            },
                           ),
                           TextFormField(
                             decoration: InputDecoration(labelText: 'Password'),
                             obscureText: true,
+                            validator: (value) {
+                              if (value!.isEmpty || value.length < 6) {
+                                return 'Password must be at least 6 characters';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _passwordController.text = value!;
+                            },
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {
+                              _submit();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(
+                                context,
+                              ).colorScheme.primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : 'SignUp'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(
+                              _isLogin
+                                  ? 'Create Account'
+                                  : 'I Already Have An Account',
+                            ),
                           ),
                         ],
                       ),
