9385eef1d694e612c020a92611d7616c6f8d8b48
Alan Varghese <alanvz2212@example.com>
2025-07-01 21:55:18 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
+    return MaterialApp(home: const AuthScreen());
   }
 }
